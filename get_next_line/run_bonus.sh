0b742b48b45546080bb2fc1892c1de6fa33a735b
Diego antolin Garcia soto <digarcia@c3r3s3.42madrid.com>
2020-08-20 20:23:35 +0200

    clean

--- get_next_line/run_bonus.sh
+++
@@ -1,1 +0,0 @@
-gcc -g main.c  get_next_line_bonus.c get_next_line_utils_bonus.c -D BUFFER_SIZE=0 && ./a.out 
--- mains/main_lstnew.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-int	main()
-{
-	t_list *lst1;
-
-	char a[10] = "Hola";
-	lst1 = ft_lstnew(a);
-	ft_printlst(lst1);
-}
